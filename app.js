72a482c62e0f569ec5964ec478751146bb5e489a
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-04-09 22:09:54 -0700

    Update

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.express
+app.use(cors());
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
-app.get('/', 
+const applicationApproved = (application) => {
+  const monthlyIntrestRate = application.intrestRate / 12;
+  const termMonths = application.termLength;
+  const payment = application.loanAmount * (monthlyIntrestRate * (1 + monthlyIntrestRate) ** termMonths) / ((1 + monthlyIntrestRate) ** termMonths - 1)
+  if (application.income / 3 > payment) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+app.post('/apply', 
   (req, res) => {
-    res.send('Hello, World!');
+    const application = {
+      email: req.body.email,
+      firstName: req.body.first,
+      lastName: req.body.last,
+      married: req.body.married,
+      dependents: req.body.dependents,
+      education: req.body.education,
+      selfEmployed: req.body.selfEmployed,
+      income: req.body.income,
+      intrestRate: req.body.intrestRate,
+      creditScore: req.body.creditScore,
+      loanAmount: req.body.loanAmount,
+      termLength: req.body.termLength,
+    }
+    console.log(applicationApproved(application));
+    res.json({approved:applicationApproved(application)});
   }
 );
 
